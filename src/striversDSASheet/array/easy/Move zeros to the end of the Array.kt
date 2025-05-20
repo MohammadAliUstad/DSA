fe1e5994d8ee3160d1797aa6d72801c54254f7a5
Mohammad <mohammadaliustad@gmail.com>
2025-05-20 10:55:12 +0530

    Update

--- src/striversDSASheet/array/easy/Move zeros to the end of the Array.kt
+++ src/striversDSASheet/array/easy/Move zeros to the end of the Array.kt
@@ -1,12 +1,39 @@
 package striversDSASheet.array.easy
 
-fun moveZerosToEnd(arr: IntArray): IntArray {
-    // Your logic will go here
+fun moveZerosToEnd1(arr: IntArray): IntArray {
+    for (i in 0 until arr.size - 1) {
+        for (i in 0 until arr.size - 1) {
+            var temp: Int
+            if (arr[i] == 0) {
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+            }
+        }
+    }
+    return arr
+}
+
+fun moveZerosToEnd2(arr: IntArray): IntArray {
+    var insertPos = 0
+
+    for (i in 0 until arr.size) {
+        if (arr[i] != 0) {
+            arr[insertPos] = arr[i]
+            insertPos++
+        }
+    }
+
+    while (insertPos < arr.size) {
+        arr[insertPos] = 0
+        insertPos++
+    }
+
     return arr
 }
 
 fun main() {
     val arr = intArrayOf(1, 0, 2, 3, 0, 4, 0, 1)
-    val result = moveZerosToEnd(arr)
+    val result = moveZerosToEnd2(arr)
     println("Output: ${result.joinToString(", ")}")
 }
