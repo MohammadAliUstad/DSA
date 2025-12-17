c92f6e93fb34b9a421e0351be64e847a437b6168
Mohammad Ali <Mohammadaliustad@gmail.com>
2025-12-17 14:58:23 +0530

    RemoveDuplicatedFromSortedArray1.kt

--- src/leetCode/RemoveDuplicatedFromSortedArray1.kt
+++ src/leetCode/RemoveDuplicatedFromSortedArray1.kt
@@ -16,6 +16,6 @@ fun removeDuplicates1(nums: IntArray): IntArray {
 }
 
 fun main() {
-    val array = intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)
+    val array = intArrayOf(1,1,2,2,3,3,4,4,5,5)
     print(removeDuplicates1(array).contentToString())
 }
